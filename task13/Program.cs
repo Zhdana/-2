09fd4f6e26eff7f3657222d1446b8c8f747d9642
Zhdana <zhdanasulina@yandex.ru>
2022-12-06 10:47:00 +0300

    second commit

---
+++ task13/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число:");
+string stringnumber = Console.ReadLine();
+
+if (stringnumber.Length <3)
+Console.WriteLine("третьей цифры нет!");
+else
+Console.WriteLine("третья цифра: " + stringnumber[2]);
