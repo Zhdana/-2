95b9a18da1b236ca5bdf441bfa5bb85026452f5a
Zhdana <zhdanasulina@yandex.ru>
2022-12-06 10:49:45 +0300

    third commit

---
+++ task15/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число:");
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("нет");
+        break;
+    case 2:
+        Console.WriteLine("нет");
+        break;
+    case 3:
+        Console.WriteLine("нет");
+        break;
+    case 4:
+        Console.WriteLine("нет");
+        break;
+    case 5:
+        Console.WriteLine("нет");
+        break;
+    case 6:
+        Console.WriteLine("да");
+        break;
+    case 7:
+        Console.WriteLine("да");
+        break;
+    default:
+        Console.WriteLine("это не день недели");
+        break;
+
+}
